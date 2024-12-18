87eb3b255eb665a773cb3135d4fe438d50b3507a
Jingxin Ye <yejingxin@google.com>
2024-12-18 05:16:20 +0000

    v0.1 clean up

--- examples/fault_tolerance/run_train.sh
+++ examples/fault_tolerance/run_train.sh
@@ -1,3 +1,4 @@
 rm -Rf results
 ft_launcher --nproc-per-node=8 --max-restarts=2 \
---fault-tol-cfg-path=fault_tol_cfg.yaml train_ddp.py --simulated_fault=rank_hanged,0
+--fault-tol-cfg-path=fault_tol_cfg.yaml train_ddp.py --epochs=10
+# --simulated_fault=rank_hanged,0
